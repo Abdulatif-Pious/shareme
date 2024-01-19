b9e24227b3fbc64400922936be9c1912523de832
Abdumannopov Abdulatif <abdulatif1215@gmail.com>
2024-01-19 22:15:08 +0500

    v-2.0.0

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wl1besem',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.js
@@ -0,0 +1,7 @@
+import  comment  from './Comment';
+import  pin  from './Pin';
+import  postedBy  from './PostedBy';
+import  save  from './Save';
+import  user  from './User';
+
+export const schemaTypes = [comment, pin, postedBy, save, user];
