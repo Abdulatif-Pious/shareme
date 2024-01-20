b1e60fe1fe2a49a50ebf1a6b776ff18de272f803
Abdumannopov Abdulatif <abdulatif1215@gmail.com>
2024-01-20 12:09:54 +0500

    restore

---
+++ src/containers/Pins.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { CreatePin, Feed, Search, PinDetail, Navbar } from '../components';
+
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className='px-2 md:px-5'>
+      
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm}  setSearchTerm={setSearchTerm} user={user && user} /> 
+      </div>
+      
+      <div className='h-full'>
+        <Routes>
+          <Route  path='/' element={<Feed />} />
+          <Route  path='/categories/:categoryId' element={<Feed /> } />
+          <Route  path='/search' element={<Search searchTerm={ searchTerm } /> } />
+          <Route  path='/pin-detail/:pinId' element={<PinDetail user={user && user} />} />
+          <Route  path='/create-pin' element={<CreatePin user={user && user}/> } />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default Pins;
